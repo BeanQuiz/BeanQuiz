0fc4b811b537181cde2efafda1a365fd4fc82062
Keaton Lawrence Naidoo <keaton@bbd.co.za>
2024-03-08 09:00:18 +0200

    FE Project Structure

---
+++ BeanQuizCli/src/main/java/za/co/bbd/beanquizcli/BeanQuizCliApplication.java
@@ -0,0 +1,13 @@
+package za.co.bbd.beanquizcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanQuizCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanQuizCliApplication.class, args);
+	}
+
+}
---
+++ BeanQuizCli/src/test/java/za/co/bbd/beanquizcli/BeanQuizCliApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.beanquizcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanQuizCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
