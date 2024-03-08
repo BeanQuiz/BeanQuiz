060bbc7d0a945fd2b4c4d90997e2e5495ded5ffe
Keaton Lawrence Naidoo <keaton@bbd.co.za>
2024-03-08 08:53:00 +0200

    BE Project Structure

---
+++ BeanQuizRestApi/src/test/java/za/co/bbd/beanquizrestapi/BeanQuizRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.bbd.beanquizrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanQuizRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
