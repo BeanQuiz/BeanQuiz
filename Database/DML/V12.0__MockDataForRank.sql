22c003d901290463baf32e2066bf229e39b71e1e
Sasha Kingwill <sasha-lee.kingwill@bbd.co.za>
2024-03-14 18:26:06 +0200

    Rank table creation and population

---
+++ Database/DML/V12.0__MockDataForRank.sql
@@ -0,0 +1,12 @@
+INSERT INTO public."Rank" ("Name", "Requirement", "FunFact") 
+VALUES 
+    ('Peanut', 10, 'Georgia is the largest producer of peanuts in the US, accounting for half of the peanuts produced annually.'),
+    ('Green Bean', 20, 'After tomatoes and peppers, green beans are the third most common home-garden vegetable.'),
+    ('Pinto Bean', 30, 'Pintos contain the most fiber of all beans and are the most popular bean consumed in the United States.'),
+    ('Chickpea', 40, 'The oldest chickpea was discovered in a cave in L''Abeurador, France dating back to 6790 BC.'),
+    ('Navy Bean', 50, 'They are native to the Americas and got their American name from the sailors who used them as a staple food in the 19th century.'),
+    ('Soy Bean', 60, 'During the Civil War, soybeans were used in place of coffee because real coffee was scarce.'),
+    ('Black Bean', 70, 'Black beans have a satiny black skin (technically dark purple) and a white centre.'),
+    ('Coffee Bean', 80, 'Contrary to the name, a coffee bean isn''t actually a bean. Rather, it''s the pit (or seed) that''s found inside the coffee cherry. The bean is removed from the fruit as part of the harvest process.'),
+    ('Cocoa Bean', 90, 'It takes a cocoa tree five years to produce its first cocoa beans (pods).'),
+    ('Cool Bean', 100, 'You are just an awesome human being!');
